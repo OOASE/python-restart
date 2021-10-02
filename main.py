4b5f37ca12feb49790d45573a9b580e58fb4621f
OOASE <89398875+OOASE@users.noreply.github.com>
2021-10-02 04:58:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,252 @@
+'''
+varaible assignment
+'''
+
+# add 'solo' to the varibale 'name'
+
+# name = "solo"
+# print(name)
+
+#===========================
+
+# assign same values to multiple variables on the same line
+# p = j = t = "disciple"
+# print(p)
+# print(j)
+# print(t)
+
+#============================
+# reuse variable names, the last assignment is printed
+# colour = "red"
+# colour = "blue"
+# print(colour)
+
+#=============================
+#valid names for variables
+
+# firstname = "Peter"
+# first_name = "Peter"
+# _firs_tname = "Peter"
+# firstName = "Peter"
+# firstname2 = "Peter"
+# FIRSTNAME = "Peter"
+# print(firstname)
+# print(first_name)
+# print(_firs_tname)
+# #Invalid names for variables
+# 2firstname = "Peter"
+# first-name = "Peter"
+# first name = "Peter"
+
+'''
+Reserved keywords
+'''
+# help('keywords')
+# from = 'England'
+# print('from')
+
+#Variable types
+
+# var = "hello world"
+# print(type(var))
+
+# var = 123
+# print(type(var))
+
+#============================
+'''
+object identity
+'''
+# score = 500
+# identity = id(score)
+# print(identity)
+
+# score = 500
+# pb = score
+# print(id(pb))
+# print(id(score))
+
+#========================================
+'''
+text type
+'''
+# strings
+# a = 'agabus was a member of the way'
+# print(type(a))
+# print(a)
+
+# ========================================
+# split a sentence
+# R = '''revelations is 
+# seeing beyond human intentions'''
+# print(R)
+
+# ========================================
+#find a character by index
+# j = 'jonah'
+# print(j[4])
+
+
+# ==========================================
+#slicing and immutable
+# s = 'shield your sword'
+# print(s[3:6])
+# s[2] = 'o'
+# print(s[2])
+
+'''
+numeric type (Integer, Float, Complex)
+'''
+# x = 123456789
+# print(type(x))
+
+# x = 0.123456789123456000000984054657889
+# print(type(x))
+# print(x)
+
+# x = 1+2j
+# print(type(x))
+
+'''
+sequence types (list, tuple, range)
+'''
+
+# list
+# x = [10, 2.5, 'hello']
+# print(type(x))
+# print(x)
+# print(x[1:3])
+# print(x[2])
+#  list is mutable; 
+# x[2] = 'phone'
+# print(x)
+
+# Tuple
+
+# x = ()
+# print(x)
+# print(type(x))
+
+# Tuple = ('David', [4, 3, 2], (1, 7, 9))
+# print(Tuple)
+# print(type)
+
+# #immutable
+# tuple[2] = 11
+
+# range
+# x = range(10)
+# for n in x:
+#   print(n)
+
+'''
+mapping type (Dictionary)
+'''
+
+# dict = {}
+# print(type(dict))
+
+# dict = {'name': 'Kunle', 'age': 33}
+# print(type(dict))
+# print(dict)
+# print(dict['name'])
+
+'''
+Set Types
+'''
+
+# set = set()
+# print(type(set))
+# set = {3.2, 'hello', (1,2,3), 'hello'}
+# print(type(set))
+# print(set)
+
+#set is immutable, cannot have same values in a variable
+
+'''
+boolean type(true or false)
+'''
+
+# print(type(true))
+# print(True == 1)
+# print(False == 0)
+# print(True + True)
+# print(not True)
+# print(not False)
+print(True and True)
+print(False and True)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
